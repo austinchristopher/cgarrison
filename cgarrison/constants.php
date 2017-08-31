cf668f3b626323d97b54e4fe6b8624d685c61de1
CHRISTOPHER L. GARRISON <cgarrison@us.ibm.com>
2017-08-31 14:57:05 -0500

    restructure repo to remove /cgarrison

--- cgarrison/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define("ABOUT_ME_ID", 83);
-define("LETS_TALK_ID", 123);
-
-?>
--- cgarrison/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_theme_support( 'post-thumbnails' );
-
-function filter_ptags_on_images($content){
-  return preg_replace('/<p>\\s*?(<a .*?><img.*?><\\/a>|<img.*?>)?\\s*<\\/p>/s', '<div class="project__image">\1</div>', $content);
-}
-add_filter('the_content', 'filter_ptags_on_images');
-?>
