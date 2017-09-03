cdc797c41f2775ec3c643a9926804faad5df7dff
CHRISTOPHER L. GARRISON <cgarrison@us.ibm.com>
2017-09-03 11:40:42 -0500

    fixed mobile menu nesting and mobile project layout issues

--- site-header.php
+++ site-header.php
@@ -12,12 +12,12 @@
 
   <!-- mobile nav -->
   <nav class="site-header__main-nav--mobile">
-    <div class="main-nav__mobile-expander"></div>
-    <div class="mobile-expander__lines">
+    <div class="main-nav__mobile-expander"><div class="mobile-expander__lines">
       <span></span>
       <span></span>
       <span></span>
-    </div>
+    </div></div>
+
     <?php wp_nav_menu(array('menu' => 'main')); ?>
   </nav>
 
