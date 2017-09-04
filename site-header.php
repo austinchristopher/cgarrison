fce0bd200d2c8e145f35bec9af92e4c4c3193d00
CHRISTOPHER L. GARRISON <cgarrison@us.ibm.com>
2017-09-04 12:00:58 -0500

    fixed what I broke when I *fixed* the mobile menu

--- site-header.php
+++ site-header.php
@@ -12,12 +12,12 @@
 
   <!-- mobile nav -->
   <nav class="site-header__main-nav--mobile">
+    </div>
     <div class="main-nav__mobile-expander"><div class="mobile-expander__lines">
       <span></span>
       <span></span>
       <span></span>
-    </div></div>
-
+    </div>
     <?php wp_nav_menu(array('menu' => 'main')); ?>
   </nav>
 
